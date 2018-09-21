be61f9161b95dc1657d3b1033f12c1022517fad8
Surkhojb <juanjo@ragecoders.com>
2018-09-21 18:57:11 +0200

    Project setup: gradle dependencies and project structure

---
+++ app/src/main/java/com/example/surkhojb/samplerepositorypattern/MyApplication.java
@@ -0,0 +1,12 @@
+package com.example.surkhojb.samplerepositorypattern;
+
+import android.app.Application;
+
+/**
+ * Created by Juanjo Berenguer.
+ * Email: surkhojb@gmail.com.
+ * Date: 21/9/18.
+ */
+
+public class MyApplication extends Application {
+}
--- app/src/main/java/com/example/surkhojb/samplerepositorypattern/ui/MainActivity.java
+++ app/src/main/java/com/example/surkhojb/samplerepositorypattern/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.surkhojb.samplerepositorypattern;
+package com.example.surkhojb.samplerepositorypattern.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.surkhojb.samplerepositorypattern.R;
 
 public class MainActivity extends AppCompatActivity {
 
