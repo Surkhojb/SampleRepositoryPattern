767a4cbe6fce7483ffa4dba80b2ddea19ea8e880
Surkhojb <juanjo@ragecoders.com>
2018-09-25 18:58:25 +0200

    Created onClick events for menu buttons

--- app/src/main/java/com/example/surkhojb/samplerepositorypattern/ui/MainActivity.java
+++ app/src/main/java/com/example/surkhojb/samplerepositorypattern/ui/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.surkhojb.samplerepositorypattern.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.surkhojb.samplerepositorypattern.R;
+
+/**
+ * Created by Juanjo Berenguer.
+ * Email: surkhojb@gmail.com.
+ * Date: 21/9/18.
+ */
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +20,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void loadPosts(View view) {
+
+    }
+
+    public void loadUsers(View view) {
+
+    }
+
+    public void loadAlbums(View view) {
+
+    }
+
+    public void loadComments(View view) {
+
+    }
+
+    public void loadPhotos(View view) {
+
+    }
+
+    public void loadTodos(View view) {
+
+    }
+
+
 }
